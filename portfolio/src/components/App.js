77acb520a3f80bfb5d9d8bd948131f54bbb5b612
Dally Wally <dallin.watac.major@gmail.com>
2018-11-08 21:17:26 -0700

    css

--- portfolio/src/components/App.js
+++ portfolio/src/components/App.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import SideNav from '../containers/side_nav';
 import DisplayPage from '../containers/display_page';
 import '../style/style.css';
+import '../style/positioners.css'
 
 
 class App extends Component {
   render() {
     return (
-      <div className='row'>
+      <div className='row h100'>
         <SideNav/>
         <DisplayPage/>
       </div>

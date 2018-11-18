35adb065be12b9a17ee34e848456fa5c4cfc3031
Dally Wally <dallin.watac.major@gmail.com>
2018-11-17 21:41:46 -0700

    more projects added

--- portfolio/src/reducers/projects.js
+++ portfolio/src/reducers/projects.js
@@ -12,8 +12,22 @@ export default () => {
         project: 'Game of Memory',
         toolsUsed: [Tools.Javascript, Tools.Bootstrap, Tools.HTML, Tools.Git, Tools.React],
         description: 'This is a game I made using React. You are presented with a grid of images and when you click on an image the grid shuffles. The object of the game is to click each image once with no repeats.',
-        createdAt: '5/20/2018',
+        createdAt: '9/8/2018',
         gitHubLink: 'https://github.com/dallinmajor/MemoryGame',
         projectLink: 'https://dallinmajor.github.io/MemoryGame/'
+    },{
+        project: 'News Scrapper',
+        toolsUsed: [Tools.Javascript, Tools.Bootstrap, Tools.HTML, Tools.Git, Tools.Cheerio, Tools.Node, Tools.Express, Tools.Mongodb],
+        description: 'This site scraps the latest news stories from KSL and allows users to comment on them.',
+        createdAt: '8/29/2018',
+        gitHubLink: 'https://github.com/dallinmajor/Newsy',
+        projectLink: 'https://infinite-sea-54900.herokuapp.com/'
+    },{
+        project: 'Cartoon Gify',
+        toolsUsed: [Tools.Javascript, Tools.Bootstrap, Tools.HTML, Tools.Git, Tools.Cheerio, Tools.Node, Tools.Express, Tools.Mongodb],
+        description: '',
+        createdAt: '5/30/2018',
+        gitHubLink: 'https://github.com/dallinmajor/Giphy-Assignment/',
+        projectLink: 'https://dallinmajor.github.io/Giphy-Assignment/'
     }]
 }
